c79026b992209db586f725ee3f306cea9f76f890
Jeffrey Soriano <jeffsor17@gmail.com>
2020-10-08 20:34:22 -0600

    styled image dropper

--- client-app/src/app/common/photoUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/photoUpload/PhotoUploadWidget.tsx
@@ -1,22 +1,29 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Col, Row } from 'antd';
 import PhotoWidgetDropzone from './PhotoWidgetDropzone';
 
 const PhotoUploadWidget = () => {
     const [files, setFiles] = useState<any[]>([]);
+
+    useEffect(() => {
+        return () => {
+            files.forEach(file => URL.revokeObjectURL(file.preview))
+        }
+    })
+
     return (
         <Fragment>
-            <Row style={{ width: '100%' }}>
+            <Row gutter={[40, 16]} align='middle' justify='center' style={{ width: '100%', textAlign: 'center' }}>
                 <Col span={8}>
-                    <p>STEP 1 - add photo</p>
+                    <p>STEP 1 - ADD Photo</p>
                     <PhotoWidgetDropzone setFiles={setFiles} />
                 </Col>
                 <Col span={8}>
-                    <p>STEP 2 - Resize Image</p>
+                    <p>STEP 2 - RESIZE IMAGE</p>
                 </Col>
                 <Col span={8}>
-                    <p>STEP 3 - Preview & Upload</p>
+                    <p>STEP 3 - PREVIEW & UPLOAD</p>
                     {files.length > 0 &&
                         <img src={files[0].preview} />}
                 </Col>
--- client-app/src/app/common/photoUpload/PhotoWidgetDropzone.tsx
+++ client-app/src/app/common/photoUpload/PhotoWidgetDropzone.tsx
@@ -1,11 +1,27 @@
+import Avatar from 'antd/lib/avatar/avatar'
 import React, { useCallback } from 'react'
 import { useDropzone } from 'react-dropzone'
+import { UploadOutlined } from '@ant-design/icons';
+import { Header } from 'semantic-ui-react';
 
 interface IProps {
     setFiles: (files: object[]) => void;
 }
 
-const PhotoWidgetDropzone: React.FC<IProps> = ({setFiles}) => {
+const dropzoneStyles = {
+    border: 'dashed 3px',
+    borderColor: '#eee',
+    borderRadius: '5px',
+    paddingTop: '30px',
+    textAlign: 'center' as 'center',
+    height: '200px'
+}
+
+const dropzoneActive = {
+    borderColor: '#1890ff'
+}
+
+const PhotoWidgetDropzone: React.FC<IProps> = ({ setFiles }) => {
     const onDrop = useCallback(acceptedFiles => {
         setFiles(acceptedFiles.map((file: object) => Object.assign(file, {
             preview: URL.createObjectURL(file)
@@ -14,13 +30,10 @@ const PhotoWidgetDropzone: React.FC<IProps> = ({setFiles}) => {
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
     return (
-        <div {...getRootProps()}>
+        <div {...getRootProps()} style={isDragActive ? { ...dropzoneStyles, ...dropzoneActive } : dropzoneStyles}>
             <input {...getInputProps()} />
-            {
-                isDragActive ?
-                    <p>Drop the files here ...</p> :
-                    <p>Drag 'n' drop some files here, or click to select files</p>
-            }
+            <UploadOutlined style={{fontSize: 60}} />
+            <Header content="Drop image here" />
         </div>
     )
 }
