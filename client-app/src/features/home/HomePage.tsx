0a1eed8d81d2c059157be4b043fd4b3d4757da82
Jeffrey Soriano <jeffsor17@gmail.com>
2020-10-20 05:18:57 -0600

    added background to home page and changed buttons

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -5,6 +5,7 @@ import { Button } from 'antd';
 import { RootStoreContext } from '../../app/stores/rootStore';
 import LoginForm from '../user/LoginForm';
 import RegisterForm from '../user/RegisterForm';
+import backgroundImg from '../../app/assets/background.jpg'
 
 const HomePage = () => {
     const rootStore = useContext(RootStoreContext);
@@ -12,44 +13,55 @@ const HomePage = () => {
     const { openModal } = rootStore.modalStore
 
     return (
-        <Container>
-            <StyledDiv>
-                <StyledDiv2>
-                    <img src="/assets/logo-black.png" alt="logo" width="60px" height="60px" />
-                    <h1 style={{ fontSize: 60,  margin: 0 }}>Reactivities</h1>
-                </StyledDiv2>
+        <BackgroundDiv>
+            <Container>
+                <StyledDiv>
+                    <StyledDiv2>
+                        <img src="/assets/logo-black.png" alt="logo" width="60px" height="60px" />
+                        <h1 style={{ fontSize: 60, margin: 0 }}>Reactivities</h1>
+                    </StyledDiv2>
 
-                {isLoggedIn && user ? (
-                    <>
-                        <h3 style={{ fontSize: 20 }}>Welcome back {user.displayName}</h3>
-                        <Link to='/activities'>
-                            <Button>Go To Activities</Button>
-                        </Link>
-                    </>
-                ) : (
+                    {isLoggedIn && user ? (
                         <>
-                            <h3 style={{ fontSize: 20 }}>Welcome To Reactivities</h3>
-                            <StyledDiv2>
-                                <Button style={{ marginRight: '10px' }} onClick={() => openModal(<LoginForm />)}>Login</Button>
-                                <Button style={{ marginLeft: '10px' }} onClick={() => openModal(<RegisterForm />)}>Sign Up</Button>
-                            </StyledDiv2>
+                            <h3 style={{ fontSize: 20 }}>Welcome back {user.displayName}</h3>
+                            <Link to='/activities'>
+                                <Button ghost>Go To Activities</Button>
+                            </Link>
                         </>
-                    )}
+                    ) : (
+                            <>
+                                <h3 style={{ fontSize: 20 }}>Welcome To Reactivities</h3>
+                                <StyledDiv2>
+                                    <Button style={{ marginRight: '10px' }} onClick={() => openModal(<LoginForm />)} ghost>Login</Button>
+                                    <Button style={{ marginLeft: '10px' }} onClick={() => openModal(<RegisterForm />)} ghost>Sign Up</Button>
+                                </StyledDiv2>
+                            </>
+                        )}
 
-            </StyledDiv>
-        </Container>
+                </StyledDiv>
+            </Container>
+        </BackgroundDiv>
     )
 }
 
 export default HomePage;
+const BackgroundDiv = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-image: url(${backgroundImg});
+    background-position: 0 -155px;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+`
 
 const Container = styled.div`
-    height: 50%; 
-    width: 50%; 
-    overflow: auto; 
+    height: 30%; 
+    width: 45%; 
     margin: auto; 
     position: absolute; 
     top: 0; left: 0; bottom: 0; right: 0;
+    overflow: hidden; 
 `
 
 const StyledDiv = styled.div`
